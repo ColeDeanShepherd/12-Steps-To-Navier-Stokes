b3d2bbcec678f97d597fb290dc9ae92af03ecb3d
Cole Dean Shepherd <cole@coledeanshepherd.com>
2018-01-14 11:57:55 -0600

    Refactored, improved time-stepping.

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,94 +2,145 @@
 
 #include <SDL.h>
 
-const int graphWidth = 300;
-const int graphHeight = 300;
-const int graphX = 10;
-const int graphY = 10;
-const float graphMinX = 0;
-const float graphMaxX = 2;
-const float graphMinY = 0;
-const float graphMaxY = 2;
-const int numPoints = 41;
-const float dx = (graphMaxX - graphMinX) / (numPoints - 1);
-std::vector<float> ys(numPoints);
-
-void init()
+struct Vector2f
 {
-    // apply initial condition
-    for(auto i = 0; i < ys.size(); i++)
-    {
-        const auto x = graphMinX + (i * dx);
-        ys[i] = ((x >= 0.5f) && (x <= 1)) ? 2 : 1;
-    }
-}
-void update()
+    float x, y;
+
+    Vector2f() {}
+    Vector2f(const float x, const float y) : x(x), y(y) {}
+};
+struct GraphMetrics
 {
-    const auto dt = 0.0025f;
-    const auto c = 1.f;
+    float width, height;
+    Vector2f pos;
+    float minX, maxX;
+    float minY, maxY;
 
-    auto newYs = ys;
+    GraphMetrics() {}
+};
 
-    for(auto i = 1; i < ys.size(); i++)
-    {
-        newYs[i] = ys[i] - (c * (dt / dx) * (ys[i] - ys[i - 1]));
-    }
-
-    ys = newYs;
-}
-void draw(SDL_Renderer* renderer)
+Vector2f graphPointToPxPoint(const GraphMetrics& graphMetrics, const Vector2f& graphPoint)
 {
-    SDL_SetRenderDrawColor(renderer, 128, 128, 128, 255);
+    const auto xPercentFromLeft = (graphPoint.x - graphMetrics.minX) / (graphMetrics.maxX - graphMetrics.minX);
+    const auto yPercentFromBottom = (graphPoint.y - graphMetrics.minY) / (graphMetrics.maxY - graphMetrics.minY);
 
-    for(auto i = 0; i < numPoints - 1; i++)
-    {
-        const auto x0 = graphMinX + (i * dx);
-        const auto y0 = ys[i];
-
-        const auto px0 = graphX + (((x0 - graphMinX) / (graphMaxX - graphMinX)) * graphWidth);
-        const auto py0 = (graphY + graphHeight) - (((y0 - graphMinY) / (graphMaxY - graphMinY)) * graphHeight);
-
-        const auto x1 = graphMinX + ((i + 1) * dx);
-        const auto y1 = ys[i + 1];
-
-        const auto px1 = graphX + (((x1 - graphMinX) / (graphMaxX - graphMinX)) * graphWidth);
-        const auto py1 = (graphY + graphHeight) - (((y1 - graphMinY) / (graphMaxY - graphMinY)) * graphHeight);
-
-        SDL_RenderDrawLine(renderer, px0, py0, px1, py1);
-    }
+    return Vector2f(
+        graphMetrics.pos.x + (xPercentFromLeft * graphMetrics.width),
+        (graphMetrics.pos.y + graphMetrics.height) - (yPercentFromBottom * graphMetrics.height)
+    );
 }
 
-int main(int argc, char * argv[])
+const int WINDOW_WIDTH = 640;
+const int WINDOW_HEIGHT = 480;
+const double FIXED_TIMESTEP = 1.0 / 60.0;
+
+class Step1
+{
+public:
+    Step1()
+    {
+        graphMetrics.width = WINDOW_WIDTH - 20;
+        graphMetrics.height = WINDOW_HEIGHT - 20;
+        graphMetrics.pos = Vector2f(10, 10);
+        graphMetrics.minX = 0;
+        graphMetrics.maxX = 2;
+        graphMetrics.minY = 0;
+        graphMetrics.maxY = 2;
+
+        dx = (graphMetrics.maxX - graphMetrics.minX) / (numPoints - 1);
+
+        ys.resize(numPoints);
+
+        // apply initial condition
+        for(size_t i = 0; i < ys.size(); i++)
+        {
+            const auto x = graphMetrics.minX + (i * dx);
+            ys[i] = ((x >= 0.5f) && (x <= 1)) ? 2.0f : 1.0f;
+        }
+    }
+    void update(const double dt)
+    {
+        const auto timeScale = 0.25;
+        const auto scaledDt = (float)(timeScale * dt);
+        const auto c = 1.f;
+
+        auto newYs = ys;
+
+        for(size_t i = 1; i < ys.size(); i++)
+        {
+            newYs[i] = ys[i] - (c * (scaledDt / dx) * (ys[i] - ys[i - 1]));
+        }
+
+        ys = newYs;
+    }
+    void draw(SDL_Renderer* renderer)
+    {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+        for(auto i = 0; i < numPoints - 1; i++)
+        {
+            const auto graphPoint0 = Vector2f(graphMetrics.minX + (i * dx), ys[i]);
+            const auto pixelPoint0 = graphPointToPxPoint(graphMetrics, graphPoint0);
+
+            const auto graphPoint1 = Vector2f(graphMetrics.minX + ((i + 1) * dx), ys[i + 1]);
+            const auto pixelPoint1 = graphPointToPxPoint(graphMetrics, graphPoint1);
+
+            SDL_RenderDrawLine(renderer, (int)pixelPoint0.x, (int)pixelPoint0.y, (int)pixelPoint1.x, (int)pixelPoint1.y);
+        }
+    }
+private:
+    GraphMetrics graphMetrics;
+    const int numPoints = 41;
+    float dx;
+    std::vector<float> ys;
+};
+
+int main(int argc, char* argv[])
 {
     bool quit = false;
     
     // init SDL
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window = SDL_CreateWindow("SDL2 line drawing",
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-    init();
+    Step1 step;
+
+    auto lastPerfCount = SDL_GetPerformanceCounter();
+    double accumulatedTime = 0;
 
     while(!quit)
     {
-        SDL_Delay(10);
+        // update timing
+        const auto curPerfCount = SDL_GetPerformanceCounter();
+        const double dt = (double)(curPerfCount - lastPerfCount) / SDL_GetPerformanceFrequency();
+        accumulatedTime += dt;
+        lastPerfCount = curPerfCount;
 
+        // handle events
         SDL_Event event;
-        SDL_PollEvent(&event);
-
-        if(event.type == SDL_QUIT)
+        while(SDL_PollEvent(&event))
         {
-            quit = true;
+            if(event.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+        }
+
+        // update
+        while(accumulatedTime >= FIXED_TIMESTEP)
+        {
+            step.update(FIXED_TIMESTEP);
+            accumulatedTime -= FIXED_TIMESTEP;
         }
 
         // clear window
-        SDL_SetRenderDrawColor(renderer, 242, 242, 242, 255);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderClear(renderer);
 
         // draw stored lines
-        update();
-        draw(renderer);
+        step.draw(renderer);
 
         // render window
         SDL_RenderPresent(renderer);
