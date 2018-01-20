85033421933e195f7da156345486bdee63884bf8
Cole Dean Shepherd <cole@coledeanshepherd.com>
2018-01-20 12:14:11 -0600

    Added step 4, refactored.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <math.h>
 
 #include <SDL.h>
 
@@ -13,8 +14,8 @@ struct GraphMetrics
 {
     float width, height;
     Vector2f pos;
-    float minX, maxX;
-    float minY, maxY;
+    double minX, maxX;
+    double minY, maxY;
 
     GraphMetrics() {}
 };
@@ -33,7 +34,7 @@ Vector2f graphPointToPxPoint(const GraphMetrics& graphMetrics, const Vector2f& g
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
 
-void renderLineGraph(SDL_Renderer* renderer, const GraphMetrics& graphMetrics, const float x0, const float dx, const std::vector<float>& ys)
+void renderLineGraph(SDL_Renderer* renderer, const GraphMetrics& graphMetrics, const double x0, const double dx, const std::vector<double>& ys)
 {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
@@ -49,12 +50,12 @@ void renderLineGraph(SDL_Renderer* renderer, const GraphMetrics& graphMetrics, c
     }
 }
 
-class Step1
+class Step1LinearConvection
 {
 public:
     const double fixedTimeStep = 1.0 / 60.0;
 
-    Step1()
+    Step1LinearConvection()
     {
         graphMetrics.width = WINDOW_WIDTH - 20;
         graphMetrics.height = WINDOW_HEIGHT - 20;
@@ -83,7 +84,7 @@ public:
 
         for(size_t i = 1; i < ys.size(); i++)
         {
-            newYs[i] = ys[i] - (c * ((float)dt / dx) * (ys[i] - ys[i - 1]));
+            newYs[i] = ys[i] - (c * (dt / dx) * (ys[i] - ys[i - 1]));
         }
 
         ys = newYs;
@@ -95,15 +96,15 @@ public:
 private:
     GraphMetrics graphMetrics;
     const int numPoints = 41;
-    float dx;
-    std::vector<float> ys;
+    double dx;
+    std::vector<double> ys;
 };
-class Step2
+class Step2NonlinearConvection
 {
 public:
     const double fixedTimeStep = 1.0 / 60.0;
 
-    Step2()
+    Step2NonlinearConvection()
     {
         graphMetrics.width = WINDOW_WIDTH - 20;
         graphMetrics.height = WINDOW_HEIGHT - 20;
@@ -126,12 +127,12 @@ public:
     }
     void update(const double dt)
     {
-        const auto timeScale = 0.25;
+        const auto timeScale = 1.0 / 4;
         auto newYs = ys;
 
         for(size_t i = 1; i < ys.size(); i++)
         {
-            newYs[i] = ys[i] - (ys[i] * ((float)(timeScale * dt) / dx) * (ys[i] - ys[i - 1]));
+            newYs[i] = ys[i] - (ys[i] * ((timeScale * dt) / dx) * (ys[i] - ys[i - 1]));
         }
 
         ys = newYs;
@@ -143,15 +144,15 @@ public:
 private:
     GraphMetrics graphMetrics;
     const int numPoints = 41;
-    float dx;
-    std::vector<float> ys;
+    double dx;
+    std::vector<double> ys;
 };
-class Step3
+class Step3Diffusion
 {
 public:
     double fixedTimeStep;
 
-    Step3()
+    Step3Diffusion()
     {
         graphMetrics.width = WINDOW_WIDTH - 20;
         graphMetrics.height = WINDOW_HEIGHT - 20;
@@ -177,13 +178,13 @@ public:
     }
     void update(const double dt)
     {
-        const auto timeScale = 0.1;
+        const auto timeScale = 1.0 / 10;
         const auto nu = 0.3f; // viscosity
         auto newYs = ys;
 
         for(size_t i = 1; i < ys.size() - 1; i++)
         {
-            newYs[i] = ys[i] + (nu * ((float)(timeScale * dt) / (dx * dx)) * (ys[i + 1] - (2 * ys[i]) + ys[i - 1]));
+            newYs[i] = ys[i] + (nu * ((timeScale * dt) / (dx * dx)) * (ys[i + 1] - (2 * ys[i]) + ys[i - 1]));
         }
 
         ys = newYs;
@@ -195,8 +196,75 @@ public:
 private:
     GraphMetrics graphMetrics;
     const int numPoints = 41;
-    float dx;
-    std::vector<float> ys;
+    double dx;
+    std::vector<double> ys;
+};
+class Step4BurgersEquation
+{
+public:
+    double fixedTimeStep;
+
+    Step4BurgersEquation()
+    {
+        graphMetrics.width = WINDOW_WIDTH - 20;
+        graphMetrics.height = WINDOW_HEIGHT - 20;
+        graphMetrics.pos = Vector2f(10, 10);
+        graphMetrics.minX = 0;
+        graphMetrics.maxX = 2 * M_PI;
+        graphMetrics.minY = 0;
+        graphMetrics.maxY = 10;
+
+        dx = (graphMetrics.maxX - graphMetrics.minX) / (numPoints - 1);
+
+        ys.resize(numPoints);
+
+        nu = 0.07;
+        fixedTimeStep = dx * nu;
+
+        // apply initial condition
+        const auto nu = 0.3; // viscosity
+        for(size_t i = 0; i < ys.size(); i++)
+        {
+            const auto x = graphMetrics.minX + (i * dx);
+
+            const auto xMinus2Pi = x - (2 * M_PI);
+            const auto e1 = -((x * x) / (4 * nu));
+            const auto e2 = -(pow(xMinus2Pi, 2) / (4 * nu));
+            const auto phi = exp(e1) + exp(e2);
+            const auto dPhiDx = -(1.0 / (2 * nu)) * ((x * exp(e1)) + (xMinus2Pi * exp(e2)));
+            ys[i] = -((2 * nu) / phi) * dPhiDx + 4;
+        }
+    }
+    void update(const double dt)
+    {
+        const auto timeScale = 1.0 / 4;
+        const auto scaledDt = timeScale * dt;
+        auto newYs = ys;
+
+        for(size_t i = 1; i < ys.size(); i++)
+        {
+            const auto iPlus1 = (i + 1) % ys.size();
+
+            newYs[i] = ys[i]
+                - (((ys[i] * scaledDt) / dx) * (ys[i] - ys[i - 1]))
+                + (((nu * scaledDt) / (dx * dx)) * (ys[iPlus1] - (2 * ys[i]) + ys[i - 1]));
+        }
+
+        // apply boundary conditions
+        newYs[0] = newYs[newYs.size() - 1];
+
+        ys = newYs;
+    }
+    void draw(SDL_Renderer* renderer)
+    {
+        renderLineGraph(renderer, graphMetrics, graphMetrics.minX, dx, ys);
+    }
+private:
+    GraphMetrics graphMetrics;
+    const int numPoints = 101;
+    double dx;
+    double nu; // viscosity
+    std::vector<double> ys;
 };
 
 int main(int argc, char* argv[])
@@ -205,13 +273,14 @@ int main(int argc, char* argv[])
     
     // init SDL
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("SDL2 line drawing",
+    SDL_Window* window = SDL_CreateWindow("12 Steps to Navier-Stokes",
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-    //Step1 step;
-    //Step2 step;
-    Step3 step;
+    //Step1LinearConvection step;
+    //Step2NonlinearConvection step;
+    //Step3Diffusion step;
+    Step4BurgersEquation step;
 
     auto lastPerfCount = SDL_GetPerformanceCounter();
     double accumulatedTime = 0;
