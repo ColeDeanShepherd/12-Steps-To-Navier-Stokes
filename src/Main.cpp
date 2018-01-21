b45e65a6a2cb3dda362f8abe9215055ee011ae92
Cole Dean Shepherd <cole@coledeanshepherd.com>
2018-01-21 14:25:12 -0600

    Added step 6.

--- src/Main.cpp
+++ src/Main.cpp
@@ -305,10 +305,7 @@ public:
     }
     void update(const double dt)
     {
-        const auto timeScale = 0.25;
         const auto scaledDt = timeScale * dt;
-
-        const auto c = 1.0;
 
         auto newHeights = heights;
 
@@ -324,27 +321,33 @@ public:
         }
 
         // apply boundary conditions
-        for(size_t i = 0; i < heights.size(); i++)
+        const double bCVal = 1;
+
+        // left
+        for(size_t i = 0; i < numY; i++)
         {
-            for(size_t j = 0; j < heights[i].size(); j++)
-            {
-                if((i == 0) || (i == (heights.size() - 1)) || (j == 0) || (j == (heights[i].size() - 1)))
-                {
-                    newHeights[i][j] = 1;
-                }
-            }
+            newHeights[i][0] = bCVal;
+        }
+        // right
+        for(size_t i = 0; i < numY; i++)
+        {
+            newHeights[i][numX - 1] = bCVal;
+        }
+        // top
+        for(size_t i = 0; i < numX; i++)
+        {
+            newHeights[0][i] = bCVal;
+        }
+        // bottom
+        for(size_t i = 0; i < numX; i++)
+        {
+            newHeights[numY - 1][i] = bCVal;
         }
 
         heights = newHeights;
     }
-    void draw(SDL_Renderer* renderer)
+    void updateHeightmap()
     {
-        if(heightMap == NULL)
-        {
-            heightMap = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, numX, numY);
-        }
-
-        // update the heightmap
         unsigned char* pixelBytes;
         int pitch;
         const auto failedLocking = SDL_LockTexture(heightMap, NULL, (void**)&pixelBytes, &pitch) != 0;
@@ -370,6 +373,15 @@ public:
         }
 
         SDL_UnlockTexture(heightMap);
+    }
+    void draw(SDL_Renderer* renderer)
+    {
+        if(heightMap == NULL)
+        {
+            heightMap = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, numX, numY);
+        }
+
+        updateHeightmap();
         
         SDL_Rect graphRect;
         graphRect.w = (int)graphMetrics.width;
@@ -383,10 +395,186 @@ private:
     const int numX = 81;
     const int numY = 81;
     const double graphMaxHeight = 3;
+    const double timeScale = 0.25;
+    const double c = 1.0;
     double dx;
     double dy;
     std::vector<std::vector<double>> heights;
     SDL_Texture* heightMap;
+};
+class Step6NonlinearConvection2D
+{
+public:
+    double fixedTimeStep;
+
+    Step6NonlinearConvection2D()
+    {
+        graphMetrics.width = WINDOW_WIDTH - 20;
+        graphMetrics.height = WINDOW_HEIGHT - 20;
+        graphMetrics.pos = Vector2f(10, 10);
+        graphMetrics.minX = 0;
+        graphMetrics.maxX = 2;
+        graphMetrics.minY = 0;
+        graphMetrics.maxY = 2;
+
+        dx = (graphMetrics.maxX - graphMetrics.minX) / (numX - 1);
+        dy = (graphMetrics.maxY - graphMetrics.minY) / (numY - 1);
+
+        const auto sigma = 0.2;
+        fixedTimeStep = sigma * dx;
+
+        initForFunction(uHeights, &uHeightMap);
+        initForFunction(vHeights, &vHeightMap);
+    }
+    void initForFunction(std::vector<std::vector<double>>& heights, SDL_Texture** heightMap)
+    {
+        heights = std::vector<std::vector<double>>(numY, std::vector<double>(numX));
+
+        // apply initial condition
+        for(size_t rowIndex = 0; rowIndex < heights.size(); rowIndex++)
+        {
+            const auto y = graphMetrics.minY + (rowIndex * dy);
+
+            for(size_t columnIndex = 0; columnIndex < heights[rowIndex].size(); columnIndex++)
+            {
+                const auto x = graphMetrics.minX + (columnIndex * dx);
+                heights[rowIndex][columnIndex] = ((x >= 0.5) && (x <= 1)) && ((y >= 0.5) && (y <= 1)) ? 2 : 1;
+            }
+        }
+
+        heightMap = NULL;
+    }
+    void applyBoundaryConditions(std::vector<std::vector<double>>& heights)
+    {
+        const double bCVal = 1;
+
+        // left
+        for(size_t i = 0; i < numY; i++)
+        {
+            heights[i][0] = bCVal;
+        }
+        // right
+        for(size_t i = 0; i < numY; i++)
+        {
+            heights[i][numX - 1] = bCVal;
+        }
+        // top
+        for(size_t i = 0; i < numX; i++)
+        {
+            heights[0][i] = bCVal;
+        }
+        // bottom
+        for(size_t i = 0; i < numX; i++)
+        {
+            heights[numY - 1][i] = bCVal;
+        }
+    }
+    void update(const double dt)
+    {
+        const auto scaledDt = timeScale * dt;
+
+        // update U
+        auto newUHeights = uHeights;
+        for(size_t i = 1; i < uHeights.size(); i++)
+        {
+            for(size_t j = 1; j < uHeights[i].size(); j++)
+            {
+                newUHeights[i][j] = uHeights[i][j]
+                    - (((uHeights[i][j] * scaledDt) / dx) * (uHeights[i][j] - uHeights[i - 1][j]))
+                    - (((vHeights[i][j] * scaledDt) / dy) * (uHeights[i][j] - uHeights[i][j - 1]));
+            }
+        }
+
+        applyBoundaryConditions(newUHeights);
+        uHeights = newUHeights;
+
+        // update V
+        auto newVHeights = vHeights;
+        for(size_t i = 1; i < vHeights.size(); i++)
+        {
+            for(size_t j = 1; j < vHeights[i].size(); j++)
+            {
+                newVHeights[i][j] = vHeights[i][j]
+                    - (((uHeights[i][j] * scaledDt) / dx) * (vHeights[i][j] - vHeights[i - 1][j]))
+                    - (((vHeights[i][j] * scaledDt) / dy) * (vHeights[i][j] - vHeights[i][j - 1]));
+            }
+        }
+
+        applyBoundaryConditions(newVHeights);
+        vHeights = newVHeights;
+    }
+    void updateHeightmap(const std::vector<std::vector<double>>& heights, SDL_Texture* heightMap)
+    {
+        unsigned char* pixelBytes;
+        int pitch;
+        const auto failedLocking = SDL_LockTexture(heightMap, NULL, (void**)&pixelBytes, &pitch) != 0;
+
+        if(failedLocking)
+        {
+            throw new std::exception("Failed locking the texture.");
+        }
+
+        for(auto rowIndex = 0; rowIndex < numY; rowIndex++)
+        {
+            for(auto columnIndex = 0; columnIndex < numX; columnIndex++)
+            {
+                const auto pixelBytesOffset = (rowIndex * pitch) + (4 * columnIndex);
+                const auto pixelValuePercent = heights[rowIndex][columnIndex] / graphMaxHeight;
+                const auto pixelValue = (unsigned char)(255 * pixelValuePercent);
+
+                pixelBytes[pixelBytesOffset + 3] = pixelValue; // R
+                pixelBytes[pixelBytesOffset + 2] = pixelValue; // G
+                pixelBytes[pixelBytesOffset + 1] = pixelValue; // B
+                pixelBytes[pixelBytesOffset + 0] = 255; // A
+            }
+        }
+
+        SDL_UnlockTexture(heightMap);
+    }
+    void draw(SDL_Renderer* renderer)
+    {
+        if(uHeightMap == NULL)
+        {
+            uHeightMap = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, numX, numY);
+        }
+
+        if(vHeightMap == NULL)
+        {
+            vHeightMap = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, numX, numY);
+        }
+
+        updateHeightmap(uHeights, uHeightMap);
+        updateHeightmap(vHeights, vHeightMap);
+
+        SDL_Rect uGraphRect;
+        uGraphRect.w = ((int)graphMetrics.width / 2) - 5;
+        uGraphRect.h = (int)graphMetrics.height;
+        uGraphRect.x = (int)graphMetrics.pos.x;
+        uGraphRect.y = (int)graphMetrics.pos.y;
+
+        SDL_RenderCopy(renderer, uHeightMap, NULL, &uGraphRect);
+
+        SDL_Rect vGraphRect;
+        vGraphRect.w = ((int)graphMetrics.width / 2) - 5;
+        vGraphRect.h = (int)graphMetrics.height;
+        vGraphRect.x = uGraphRect.x + uGraphRect.w + 10;
+        vGraphRect.y = (int)graphMetrics.pos.y;
+
+        SDL_RenderCopy(renderer, vHeightMap, NULL, &vGraphRect);
+    }
+private:
+    GraphMetrics graphMetrics;
+    const int numX = 81;
+    const int numY = 81;
+    const double graphMaxHeight = 3;
+    const double timeScale = 0.25;
+    const double c = 1.0;
+    double dx;
+    double dy;
+    std::vector<std::vector<double>> uHeights;
+    std::vector<std::vector<double>> vHeights;
+    SDL_Texture* uHeightMap;
+    SDL_Texture* vHeightMap;
 };
 
 int main(int argc, char* argv[])
@@ -403,7 +591,8 @@ int main(int argc, char* argv[])
     //Step2NonlinearConvection1D step;
     //Step3Diffusion1D step;
     //Step4BurgersEquation1D step;
-    Step5LinearConvection2D step;
+    //Step5LinearConvection2D step;
+    Step6NonlinearConvection2D step;
 
     auto lastPerfCount = SDL_GetPerformanceCounter();
     double accumulatedTime = 0;
